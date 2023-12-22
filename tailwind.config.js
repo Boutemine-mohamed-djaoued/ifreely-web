032827746c3ba992464379e7f8cc919901152678
Djawad-boutemine <sleepwell@pop-os.localdomain>
2023-12-22 17:20:12 +0100

    initail stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,12 +19,12 @@ export default {
       500: "clamp(1.35rem, calc(1.21rem + 0.69vw), 1.75rem)",
       600: "clamp(1.62rem, calc(1.37rem + 1.24vw), 2.33rem)",
       700: "clamp(1.94rem, calc(1.54rem + 2.03vw), 3.11rem)",
-      800: " clamp(2.33rem, calc(1.7rem + 3.15vw), 4.14rem)",
+      800: "clamp(2.33rem, calc(1.7rem + 3.15vw), 4.14rem)",
       900: "clamp(2.8rem, calc(1.85rem + 4.74vw), 5.52rem)",
     },
-    fontFamily:{
-      sans:['Poppins','sans-serif'] ,
-    } ,
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
